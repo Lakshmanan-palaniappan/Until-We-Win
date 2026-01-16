dad484bcab44a48a389fc75dee8aed5a30ad6105
Lakshmanan Palaniappan <154072727+Lakshmanan-palaniappan@users.noreply.github.com>
2026-01-16 10:34:01 +0530

    TwoSum-Leo

---
+++ Letchu/TwoSum.java
@@ -0,0 +1,15 @@
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int i = 0;
+        while (i < nums.length) {
+            int x = target - nums[i];
+            if (map.containsKey(x)) {
+                return new int[] { i, map.get(x) };
+            }
+            map.put(nums[i], i);
+            ++i;
+        }
+        return new int[] {};
+    }
+}
